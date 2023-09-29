918a90bf54532efb8c9e5e239f5187022a3d9083
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2023-09-29 19:34:57 +0200

    indexators and some collections work

---
+++ Collections/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+
+            List<int> numbers = new List<int>(4) {43, 23, 11, 20 };
+
+            numbers.Add(13);
+            numbers.Add(21);
+
+
+            List<int[]> arrays = new List<int[]>(3) {new int[3] {1,2,3}, new int[3] {4,5,6} };
+            arrays.Add(new int[3] { 7, 8, 9 });
+
+            List<(int, string)> corteges = new List<(int, string)>(3) { (23, "dog"), (22, "cat"), (24, "summer") };
+            corteges.Add((27, "house"));
+
+
+
+
+
+            //List<object> objects = new List<object>();
+
+            //object[] obj;
+
+            //objects.Add(10);
+            //objects.Add('g');
+            //objects.Add("jlkl");
+            //objects.Add((short)24);
+            //objects.Add(new Object());
+
+            //obj = objects.ToArray();
+          
+
+            //int[]numbers = new int[10];
+
+            //for(int i = 0; i < 10; i++)
+            //{
+            //    numbers[i] = (random.Next(0, 28));              
+            //}
+
+            //foreach(int item in numbers)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+          
+        }
+    }
+}
---
+++ Lesson2909/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace Lesson2909
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+
+
+            // Индексатор
+            //ArrayOfStrings arr = new ArrayOfStrings();
+            //arr[2] = "Hello"; 
+        }
+    }
+
+
+
+    // Разбор задачи №11 (ДЗ)
+    class BaseClass
+    {
+        public int field;
+
+        public virtual int this[int number]
+        {
+            get
+            {
+                int sumCounter = 0;
+                int temp = field;
+
+                for(int i = 0; temp >0; i++)
+                {
+                    field /= 10;
+                    sumCounter++;
+                }
+
+                int[] arr = new int[sumCounter]; // наполнили цифрами в обратном порядке
+                temp = field;
+
+                for(int i = 0; i < temp; i++)
+                {
+                    arr[i] = temp % 10;
+                    field /= 10;
+                }
+
+                Array.Reverse(arr); // не забыть подумать
+
+                return arr[number];
+            }
+        }
+    }
+
+    //class ChildClass:BaseClass
+    //{
+    //    public int field2;
+
+    //    public override int this[int number] 
+    //    { 
+            
+    //    }
+    //}
+
+
+
+    // Небольшой разбор индексатора
+    //class ArrayOfStrings
+    //{
+    //    private string[] _array = new string[10];
+
+    //    public string this[int index]
+    //    {
+    //        get
+    //        {
+    //            return _array[index];
+    //        }
+    //        set
+    //        {
+    //            if (index < 0 || index >= _array.Length)
+    //            {
+    //                throw new IndexOutOfRangeException("Введен неверный индекс");
+    //            }
+    //            _array[index] = value;
+    //        }
+    //    }
+    //}
+}
