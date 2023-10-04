96c1c275d5cf652dac91163ef7504e9622090513
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2023-10-04 19:50:38 +0200

    boxing-unboxing

--- Lesson0410/Program.cs
+++ Lesson0410/Program.cs
@@ -2,11 +2,32 @@
 {
     internal class Program
     {
-        
+        // immutable (неизменяемость) есть ТОЛЬКО у значимых типов и у String, который содержит свой адрес(ссылку)
+        // в стеке, а свой объект в виде массива char - в куче, т.е. он имеет свойства как стека, так и кучи
 
+
+        // Boxing-unboxing
+        // boxing - упаковка значимого типа в ссылочный тип
+        // unboxing - распаковка ссылочного типа в значимый тип
+        // проблемы - расход памяти, нагрузка на производительность
         static void Main(string[] args)
         {
-            
+            int a = 6; // Число находится в стеке
+
+            object referenceQbject = a; // Упаковали его в ссылочный тип, сделали из него класс. Возникла УТЕЧКА ПЯМЯТИ
+
+            int newNumber = (int) referenceQbject; // Распаковали его обратно в значимый тип
+
+            Person man = new Person();
+
+            object newMan = man;
+
+            Person someMan = (Person)newMan;
+        }
+
+        class Person // Все что касается этого класса находися в куче
+        {
+            public int Id { get; set; }
         }
     }
 }
