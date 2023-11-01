eee94ae050e3009bf688fdc7dc95f05224d014d9
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2023-11-01 20:02:23 +0200

    key word IS

--- Lesson0111/Program.cs
+++ Lesson0111/Program.cs
@@ -12,9 +12,9 @@
             Thread three = new Thread(CountToCentury);
             Thread four = new Thread(CountToCentury);
 
-            Thread first = new Thread(new ParameterizedThreadStart(Printer));
-            Thread second = new Thread(Printer);
-            Thread third = new Thread(message => { Console.WriteLine(message);});
+            Thread first = new(new ParameterizedThreadStart(Printer));
+            Thread second = new(Printer);
+            Thread third = new(message => { Console.WriteLine(message);});
 
             first.Start("Привет");
             second.Start("Привет");
@@ -26,14 +26,61 @@
             four.Start(new int[] { 4, 2, 34, 11, 20, 21});
 
 
+            CustomArray myArr = new CustomArray(5);
+            myArr[0] = 5.5;
+
+            Thread firstThread = new(Displayer);
+            Thread secondThread = new(Displayer);
+            Thread thirdThread = new(Displayer);
+
+            firstThread.Start(myArr);
+            secondThread.Start("Удача");
+            thirdThread.Start(new int[] { 4, 5, 6, 7, 2, 4, 5, 6, 7, 4 });
+
+
         }
 
-        public static void Printer(object obj)
+
+
+        public static void Displayer(object? obj)
         {
+            if(obj is CustomArray)
+            {               
+                CustomArray array = (CustomArray)obj;
+
+                for(int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+            else if(obj is string)
+            {
+                string line = (string) obj;
+                Console.WriteLine(line);
+            }
+            else if(obj is int[])
+            {
+                int[] array = (int[])obj;
+                int sum = 0;
+                foreach(int i in array)
+                {
+                    sum += i;
+                }
+                Console.WriteLine(sum);
+            }
+        }
+
+
+        public static void Printer(object? obj)
+        {
+            if (obj is not string)
+            {
+
+            }
             Console.WriteLine(obj);
         }
 
-        public static void CountToCentury(object obj)
+        public static void CountToCentury(object? obj)
         {
            if(obj is int[])
            {
@@ -70,5 +117,35 @@
         public string CarType { get; set; }
         public double Speed { get; set; }
 
+    }
+
+    class CustomArray
+    {
+        private double[] _data;
+
+        public int Length
+        {
+            get
+            {
+                return _data.Length;
+            }
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                return _data[index];
+            }
+            set
+            {
+                _data[index] = value;
+            }
+        }
+
+        public CustomArray(int length) 
+        {
+            _data = new double[length];
+        }
     }
 }
