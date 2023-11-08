d95971acec7b024f839669e77e5ad6260ce685a6
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2023-11-08 20:03:31 +0200

    events simple repeat

---
+++ EventsSimple/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace EventsSimple
+{
+    
+    internal class Program
+    {
+        public delegate void MyDelegateForEvent(string message);
+        public static event MyDelegateForEvent MyEvent;
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5] {1,2,3,4,5};
+           
+            MyEvent += (message) => { Console.WriteLine(message); };
+
+            foreach(int item in numbers)
+            {
+                if(item % 2 == 0)
+                {
+                    MyEvent?.Invoke("Четное");                    
+                }
+            }
+        }
+    }
+    
+}
