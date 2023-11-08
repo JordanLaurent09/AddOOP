8d95173ffde704f05d7d9ce1036faea51c7e5768
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2023-11-08 19:36:15 +0200

    inheritance repeat

---
+++ RepeatLessonTwo/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace RepeatLessonTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Person
+    {        
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+
+    class User : Person
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public User(int id,  string userName, int age) : base(userName, age)
+        {
+            Id = id;
+        }
+    }
+}
