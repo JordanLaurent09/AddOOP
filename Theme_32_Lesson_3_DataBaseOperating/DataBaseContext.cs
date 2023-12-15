d04b7c49e08a3e1d488f80cae80c186502ad9975
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2023-12-15 21:09:41 +0200

    db lesson 3

---
+++ Theme_32_Lesson_3_DataBaseOperating/DataBaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Theme_32_Lesson_3_DataBaseOperating
+{
+    public class DataBaseContext:DbContext
+    {
+        public DbSet<Person> People => Set<Person>();
+
+        public DataBaseContext()
+        {
+            Database.EnsureDeleted();  // Удаление БД
+            Database.EnsureCreated();  // Создание БД
+
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=TitanicPassengers.db");
+        }
+    }
+}
---
+++ Theme_32_Lesson_3_DataBaseOperating/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Theme_32_Lesson_3_DataBaseOperating
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var context = new DataBaseContext())
+            {
+                // context.Database.CanConnectAsync() // Асинхронная проверка подключения
+                if (context.Database.CanConnect()) // Проверка подключения к БД
+                {
+                    context.Database.EnsureDeleted(); // Данные методы можно определять и в главном методе main,
+                    context.Database.EnsureCreated(); // а не только в конструкторе DataBaseContext
+
+                    context.Database.EnsureDeletedAsync(); // Данные методы можно определять и в главном методе main
+                    context.Database.EnsureCreatedAsync(); // а не только в конструкторе DataBaseContext
+                }
+            }
+        }
+    }
+}
