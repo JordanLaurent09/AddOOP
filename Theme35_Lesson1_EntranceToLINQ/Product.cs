2cd413dd2ee3302dca572f0d6ec590dc454a21a0
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2024-01-12 19:12:23 +0200

    LINQ basics

---
+++ Theme35_Lesson1_EntranceToLINQ/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace Theme35_Lesson1_EntranceToLINQ
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(int id, string name, string description, double price, int quantity)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ Theme35_Lesson1_EntranceToLINQ/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Theme35_Lesson1_EntranceToLINQ
+{
+    // интерфейс IEnumerable
+    // 1. Where(p => p.property) Выборка данных по условию
+
+    // DB-Style: FROM item IN items
+    //           WHERE item_condition
+    //           SELECT item
+
+    // LINQ-запросы - отложенные и выполняются только при обращении к коллекции(например, с помощью foreach)
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product(0, "Apple", "Red delicious", 1.50, 10),
+                new Product(1, "Banana", "Yellow delicious", 1.20, 13),
+                new Product(2, "Lollipop", "Lollipop-lollipop", 1.40, 20),
+                new Product(3, "Lemon", "Yellow delicates", 1.70, 21),
+                new Product(4, "Orange", "Orange pleasure", 2.50, 22)
+            };
+
+            // Выборка данных по условию в лямбда-выражении
+            //IEnumerable<Product> items = products.Where(p => p.Price < 2.0);
+
+            //foreach(Product item in items)
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+
+            // Запрос на LINQ
+            IEnumerable<string> result = from good in products
+                                         where good.Price < 2.0
+                                         select good.Name;
+
+            // Запрос на LINQ методах расширения
+            IEnumerable<string> extensionMethodsResult = products.Where(good => good.Price < 2.0).Select(good => good.Name);
+            
+        }
+    }
+}
