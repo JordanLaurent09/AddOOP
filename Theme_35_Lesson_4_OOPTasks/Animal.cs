89f58b571ad02e649eabeafc2d892cef8920590e
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2024-01-17 20:41:37 +0200

    animals

---
+++ Theme_35_Lesson_4_OOPTasks/Animal.cs
@@ -0,0 +1,51 @@
+﻿namespace Theme_35_Lesson_4_OOPTasks
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        public string Species { get; set; }
+        public double Weight { get; set; }
+
+
+        public Animal(string name, string species, double weight):this(name, species)
+        {            
+            Weight = weight;
+        }
+
+        public Animal(string name, string species)
+        {
+            Name = name;
+            Species = species;
+        }
+
+        public Animal()
+        {
+
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("SOUND");
+        }
+
+        public void MakeSound(string animalSound)
+        {
+            Console.WriteLine(animalSound);
+        }
+
+        public void Feed()
+        {
+            Console.WriteLine("MnomMnomMnom");
+        }
+
+        public static void FeedAll(Animal[] animals)
+        {
+            foreach(Animal animal in animals)
+            {
+                animal.Feed();
+            }
+        }
+
+    }
+}
---
+++ Theme_35_Lesson_4_OOPTasks/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Theme_35_Lesson_4_OOPTasks
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal snake = new Animal() { Name = "Plissken", Species = "ColdBlooded", Weight = 6};
+
+            Animal hound = new Animal() { Name = "Bloodhound", Species = "MilkEaters", Weight = 16 };
+
+            Animal spider = new Animal() { Name = "Shalob", Species = "Chlenistonogye", Weight = 200 };
+
+
+            Console.WriteLine($"{snake.Name} {snake.Species} {snake.Weight}");
+            Console.WriteLine($"{hound.Name} {hound.Species} {hound.Weight}");
+            Console.WriteLine($"{spider.Name} {spider.Species} {spider.Weight}");
+
+
+            snake.MakeSound("Hisss");
+            hound.MakeSound("Guff");
+            spider.MakeSound("???");
+
+            Animal[] animals = new Animal[] { snake, hound, spider };
+
+            Animal.FeedAll(animals);
+        }
+    }
+}
